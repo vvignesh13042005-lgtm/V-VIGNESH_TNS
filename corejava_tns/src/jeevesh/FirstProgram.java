a0c7d841adf0e8ff6332a7f45a6177b3719749cd
vigne <vigne@VIGNESH_53>
2025-08-08 13:55:38 +0530

    commit 2

--- corejava_tns/src/jeevesh/FirstProgram.java
+++
@@ -1,9 +0,0 @@
-//Demonstration of simple java program
-package jeevesh;
-import java.util.LinkedList ; //all subpackages
-
-public class FirstProgram {
-	public static void main(String[] args) {
-		System.out.println("Hello World");
-	}
-}
